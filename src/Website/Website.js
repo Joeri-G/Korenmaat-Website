d9735f8ae02abcfd77ac7abbdfbee1808881a326
Joeri-G <3677joeri@gmail.com>
2020-08-11 00:37:23 +0200

    Added Credits

--- src/Website/Website.js
+++ src/Website/Website.js
@@ -5,8 +5,11 @@ import Main from './Components/Main';
 import Footer from './Components/Footer';
 import './css/Master.css';
 
-
 export default class Website extends Component {
+  constructor(props) {
+    super(props);
+    setTimeout(() => console.log("By Joeri Geuzinge. https://www.joerigeuzinge.nl"), 1000);
+  }
   render() {
     return (
       <React.Fragment>
