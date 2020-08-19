5b26746043b76b147192ff4a76c450ab41e28fc2
Joeri-G <3677joeri@gmail.com>
2020-08-19 19:32:50 +0200

    Updated navbar message

--- src/Website/Components/Navbar.js
+++ src/Website/Components/Navbar.js
@@ -13,7 +13,7 @@ export default class Navbar extends Component {
         </div>
         <div className="navbarLogoRight">
           <span className="navbarTitleRight">
-            Biologische (web) winkel met bezorg service
+            Biologische (web) winkel met bezorgservice
           </span>
           {/* <img src={KorenmaatLogo} alt="Logo" /> */}
         </div>
